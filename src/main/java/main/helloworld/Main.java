1febd9c2cfd28312890636827aa684ea9bc44496
bgd988 <zemun88@gmail.com>
2020-02-06 19:58:56 +0100

    vezbanje zadataka

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -8,17 +8,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         System.out.println("Hello World");
         System.out.println("Bojan");
-        
-      //  int age = 5;
-    //    if (age > 18) {
-    //        System.out.println("Osoba je punoletna");
-    //    } else {
-    //        System.out.println("Osoba je maloletna");
-    //    }
 
+        int i, j;
+
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j <= 10; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+//        for (i = 0; i < 10; i++) {
+//            for (j = 0; j <= 10; j++) {
+//                System.out.print(" ( " + i + " , " + j + ")");
+//            }
+//            System.out.println();
+//        }
+        
+         for (i = 1; i < 11; i++) {
+            for (j = 1 ; j <= 11; j++) {
+                System.out.print(" ( " + i + " , " + j + ")");
+            }
+            System.out.println();
+        }
+
+        //  int age = 5;
+        //    if (age > 18) {
+        //        System.out.println("Osoba je punoletna");
+        //    } else {
+        //        System.out.println("Osoba je maloletna");
+        //    }
 //        System.out.print("Unesite godine ");
 //        Scanner input = new Scanner(System.in);
 //        int userAge = input.nextInt();
@@ -37,10 +60,9 @@ public class Main {
 //            System.out.println("Osoba je punoletna");
 //        } 
 //        
-             
-            //else {
+        //else {
         //    System.out.println("Osoba je maloletna");
-       // }
+        // }
 //            
 //       System.out.print("Unesite redni broj dana u nedelji (1-7):  ");
 //        Scanner input = new Scanner(System.in);
@@ -52,14 +74,30 @@ public class Main {
 //        Switch(dayNumber);
 //          
 //           System.out.println("Nedelja"); 
-
-        int x = 1;
-        while(x < 10) {
-            System.out.println(x);
-            x = x + 1;
-        }
+//        int x = 1;
+//        while(x < 10) {
+//            System.out.println(x);
+//            x = x + 1;
+//        }
+//        
+//         System.out.println("While loop2: ");
+//        
+//      x = 10;
+//        
+//        while (x > 0) {
+//             System.out.println(x);
+//             x--;
+//        }
+//        
+//         System.out.println("Do While loop");
+//         do {
+//             x--;
+//         }  while(x>15);
+//         
+//         
+//         for (int i = 30; i > 19; i--) {
+//            System.out.println(i);
+//        }
     }
-    
-    
-    
+
 }

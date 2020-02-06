0db1b7c6b01451d0bf4794f305f67ee6728fbd9a
bgd988 <zemun88@gmail.com>
2020-02-06 18:39:37 +0100

    Dodati fajlovi

---
+++ src/main/java/main/helloworld/Main.java
@@ -0,0 +1,65 @@
+package main.helloworld;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        System.out.println("Bojan");
+        
+      //  int age = 5;
+    //    if (age > 18) {
+    //        System.out.println("Osoba je punoletna");
+    //    } else {
+    //        System.out.println("Osoba je maloletna");
+    //    }
+
+//        System.out.print("Unesite godine ");
+//        Scanner input = new Scanner(System.in);
+//        int userAge = input.nextInt();
+//        
+//        System.out.println("Korisnik je uneo: " + userAge);
+//        
+//          if (userAge <0 && userAge > 150 ) {
+//            System.out.println("Invalid");
+//          } else    if (userAge >=0 && userAge<= 5) {
+//            System.out.println("Osoba je beba");
+//        }  else  if (userAge<= 11) {
+//            System.out.println("Osoba je dete");
+//        }else  if (userAge<= 17) {
+//            System.out.println("Osoba je tinejdzer"); 
+//        }else  if (userAge <= 150) {
+//            System.out.println("Osoba je punoletna");
+//        } 
+//        
+             
+            //else {
+        //    System.out.println("Osoba je maloletna");
+       // }
+//            
+//       System.out.print("Unesite redni broj dana u nedelji (1-7):  ");
+//        Scanner input = new Scanner(System.in);
+//        
+//        int dayNumber = input.nextInt();
+//        
+//        System.out.println(dayNumber + "");
+//        
+//        Switch(dayNumber);
+//          
+//           System.out.println("Nedelja"); 
+
+        int x = 1;
+        while(x < 10) {
+            System.out.println(x);
+            x = x + 1;
+        }
+    }
+    
+    
+    
+}
